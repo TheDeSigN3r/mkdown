14a33bcd2ef91d62136f2671a089fbde99b2005b
Negative <negative_amir@yahoo.com>
2016-06-12 12:53:08 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import sys
+reload(sys)
+sys.setdefaultencoding("utf-8")
+import telebot
+from telebot import types
+
+TOKEN = 'YOUR TOKEN' # محل توکن شما 
+bot = telebot.TeleBot(TOKEN)
+
+@bot.inline_handler(lambda query: len(query.query.split()) == 1) #query Text 
+def qq(q):
+    text = q.query
+    bold = types.InlineQueryResultArticle('1', 'Bold', types.InputTextMessageContent('*{}* \n @Taylor_Team'.format(text), parse_mode="Markdown"))
+    code = types.InlineQueryResultArticle('2', 'Code', types.InputTextMessageContent('```{}``` \n @Taylor_Team'.format(text), parse_mode="Markdown"))
+    italic = types.InlineQueryResultArticle('3', 'Italic', types.InputTextMessageContent('_{}_ \n @Taylor_Team'.format(text), parse_mode="Markdown"))
+                
+
+    bot.answer_inline_query(q.id, [bold, code, italic], cache_time=1)
+    
+    
+bot.polling(True)
+
+#new Update Soon
